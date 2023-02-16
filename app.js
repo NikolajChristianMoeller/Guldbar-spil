a9294dd32563dc88a0e67a161090ed284f60f474
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-02-16 16:20:52 +0100

    Ændringer
    Ændringer

---
+++ app.js
@@ -0,0 +1,43 @@
+var PersonSaved = 0;
+var gameElementsCola = document.getElementById("game_elements_cola");
+
+function ColaClicked() {
+  //when clicked on person personsaved gets incremented
+  PersonSaved++;
+
+  console.log(PersonSaved);
+  gameElementsCola.style.visibility = "hidden";
+}
+
+var PersonSaved = 0;
+var gameElementsFanta = document.getElementById("game_elements_fanta");
+
+function FantaClicked() {
+  //when clicked on person personsaved gets incremented
+  PersonSaved++;
+
+  console.log(PersonSaved);
+  gameElementsFanta.style.visibility = "hidden";
+}
+
+var PersonSaved = 0;
+var gameElementsØl = document.getElementById("game_elements_øl");
+
+function ØlClicked() {
+  //when clicked on person personsaved gets incremented
+  PersonSaved++;
+
+  console.log(PersonSaved);
+  gameElementsØl.style.visibility = "hidden";
+}
+
+var PersonSaved = 0;
+var gameElementsBærbar = document.getElementById("game_elements_bærbar");
+
+function BærbarClicked() {
+  //when clicked on person personsaved gets incremented
+  PersonSaved++;
+
+  console.log(PersonSaved);
+  gameElementsBærbar.style.visibility = "hidden";
+}
